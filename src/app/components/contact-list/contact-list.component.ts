b0c61020215d369283f8e392fb83a970825aa328
Vitaliy Cholan <vitaliy.cholan.31@gmail.com>
2025-02-18 21:38:10 +0200

    Display contact list and add search bar

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '../../models/contact.model';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  styleUrl: './contact-list.component.css',
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
+  searchTerm: string = '';
 
+  constructor(private contactService: ContactService) {}
+
+  ngOnInit() {
+    this.contacts = this.contactService.getContacts();
+  }
+
+  get filteredContacts(): Contact[] {
+    return this.contacts.filter(
+      (contact) =>
+        contact.firstName
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase()) ||
+        contact.lastName
+          .toLowerCase()
+          .includes(this.searchTerm.toLowerCase()) ||
+        contact.phone.includes(this.searchTerm)
+    );
+  }
 }
